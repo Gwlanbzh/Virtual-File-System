a2ec17b85d7d92c88753281189aabd31138e5073
Gwlanbzh <62286593+Gwlanbzh@users.noreply.github.com>
2020-03-18 16:10:53 +0100

    Append external specifications
    Append external specifications as comments.

--- fs.py
+++ fs.py
@@ -1,4 +1,31 @@
+# Formatting a virtual partition.
+
 def init(PATH: str) -> int:
     """Initializes a virtual partition.
     """
     
+# Mounting a virtual partition.
+
+
+
+# Opening and cllosing of a file.
+
+
+
+# Reading and writing to a file.
+
+
+
+# positionning the cursor to the beginning of a file.
+
+
+
+# Reading a file's metadatas.
+
+
+
+# Reading a directory's content.
+
+
+
+# Entry appending and deletion in a directory.
