d8def6f22f4a2d3805d4695e7307d50040ff83d2
Gab_351 <gabriel@blaisot.org>
2020-03-18 11:21:38 +0100

    add controleur.py functions headers

---
+++ controleur.py
@@ -0,0 +1,11 @@
+def seek(name: str, addr_block: int):
+    """move the cursor on the [addr_block] th block of 512o.
+    """
+
+def read():
+    """read the block of 512o on which the cursor is.
+    """
+
+def write(data: str):
+    """write [data] in the block of 512o on which the cursor is.
+    """
