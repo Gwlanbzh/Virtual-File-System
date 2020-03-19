8a413f7715000f0b06d4be6237aaae7ea9411a35
Gwlanbzh <62286593+Gwlanbzh@users.noreply.github.com>
2020-03-19 15:53:03 +0100

    Update fs.py

--- fs.py
+++ fs.py
@@ -3,57 +3,68 @@
 def init(PATH: str) -> int:
     """Initializes a virtual partition, which needs to be unmounted.
     """
+    pass
     
 # Mounting a virtual partition.
 
 def mount_dsk(PATH: str) -> int:
     """mounts the virtual partition.
     """
+    pass
 
 def umount_dsk(MNT: str) -> int:
     """umounts the virtual partition.
     """
+    pass
 
 # Reading a directory's content.
 
-ls(DIR: str) -> str:
+def ls(DIR: str) -> str:
     """Read a directory's content.
     """
+    pass
 
 # Entry appending and deletion in a directory.
 
-mkdir(parent_dir, NAME: str) -> int:
+def mkdir(parent_dir, NAME: str) -> int:
     """Creates an empty directory.
     """
+    pass
 
 # Reading a file's metadatas.
 
-read_meta(FILE: str) -> meta:
+def read_meta(FILE: str) -> meta:
     """Returns a file's metadata as a struct called "meta".
     """
+    pass
 
 # Opening and closing of a file.
 
 def fopen(FILE: str, mode: char) -> file_ptr:
     """Opens a file.
     """
+    pass
 
 def fclose(f: file_ptr) -> int:
     """Closes a file.
     """
+    pass
 
 # positionning the cursor to the beginning of a file.
 
 def seek_file_beg(FILE: str) -> int:
     """position the cursor at the beginning of a file.
     """
+    pass
 
 # Reading and writing to a file.
 
 def fread(f: file_ptr, nb_blocks: int) -> bytes:
     """Reads nb_blocks blocks of 512 bytes from the file f.
     """
+    pass
 
 def fwrite(f: file_ptr, to_write) -> int:
     """write some bytes to a file.
     """
+    pass
