6a90167c23c4e43f19b7f49c98163765c627a4bc
Gwlanbzh <62286593+Gwlanbzh@users.noreply.github.com>
2020-03-19 12:05:58 +0100

    Update fs.py

--- fs.py
+++ fs.py
@@ -1,16 +1,38 @@
 # Formatting a virtual partition.
 
 def init(PATH: str) -> int:
-    """Initializes a virtual partition.
+    """Initializes a virtual partition, which needs to be unmounted.
     """
     
 # Mounting a virtual partition.
+
+def mount_dsk(PATH: str) -> int:
+    """mounts the virtual partition
+    """
+
+def umount_dsk(MNT: str) -> int:
+    """umounts the virtual partition
+    """
+
+# Reading a directory's content.
+
+
+
+# Entry appending and deletion in a directory.
+
+
+
+# Reading a file's metadatas.
 
 
 
 # Opening and closing of a file.
 
+def fopen(FILE_START: int, FILE_END: int) -> file_ptr:
+    """Opens a file.
+    """
 
+def
 
 # Reading and writing to a file.
 
@@ -20,12 +42,3 @@ def init(PATH: str) -> int:
 
 
 
-# Reading a file's metadatas.
-
-
-
-# Reading a directory's content.
-
-
-
-# Entry appending and deletion in a directory.
