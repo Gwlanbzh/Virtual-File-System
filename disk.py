943582e281c126493cfefd0c89b584cf9960cda1
Gab_351 <gabriel@blaisot.org>
2020-03-26 23:24:44 +0100

    add seek(-1), optimize read() and add write() function in disk.py

--- disk.py
+++ disk.py
@@ -18,6 +18,9 @@ class disk(object):
         """
         if self.nb_blocks() <= addr_block:
             return -1
+        if addr_block == -1:
+            self.cursor = self.nb_blocks() - 1
+            return 0
         self.cursor = addr_block
         return 0
 
@@ -26,12 +29,25 @@ class disk(object):
         """
         if self.cursor + nb_blocks > self.nb_blocks():
             raise IOError("end of file")
-        with open(self.name, "r") as file:
-            data = file.read()
-            return data[self.cursor * 512 :(self.cursor + nb_blocks) * 512].encode()
+        with open(self.name, "rb") as file:
+            file.seek(self.cursor * 512)
+            return file.read(nb_blocks * 512)
 
 
     def write(self, nb_blocks: int, data: str) -> int:
         """write [data] in the block of 512 o on which the cursor is.
         """
-        pass
+        data = data.encode()
+        if len(data) != 512 * nb_blocks:
+            data += b"\x00" * (nb_blocks * 512 - (len(data) % 512))
+        with open(self.name, "rb") as file:
+            before = file.read(self.cursor * 512)
+            erase = file.read(nb_blocks)
+            if self.nb_blocks() - self.cursor * 512 - nb_blocks > 0:
+                after = file.read(self.nb_blocks() - self.cursor * 512 - nb_blocks)
+            else:
+                after = b""
+        with open(self.name, "wb") as file:
+            file.seek(0)
+            file.write(before + data + after)
+        return 0
