1cf0c79c42217a6e4123917018ecc6bbce0f1cbe
Gab_351 <gabriel@blaisot.org>
2020-04-09 12:10:21 +0200

    optimize test_seek.py

--- test/test_disk/test_seek.py
+++ test/test_disk/test_seek.py
@@ -7,7 +7,7 @@ import sys
 from tempfile import mkdtemp
 
 sys.path.insert(
-    0, osp.abspath(osp.join(osp.dirname(osp.abspath(__file__)), "..",".."))
+    0, osp.abspath(osp.join(osp.dirname(osp.abspath(__file__)), "..", ".."))
 )
 from disk import Disk
 
@@ -27,7 +27,6 @@ class TestDiskSeek(object):
         # Check we have created a file with 2048 blocks of 512 bytes
         assert file_stats.st_size == (2048 * 512)
 
-
     def test_seek_beginning(self):
         self.p.seek(0)
         assert self.p.cursor == 0
@@ -35,7 +34,6 @@ class TestDiskSeek(object):
     def test_seek_end(self):
         self.p.seek(-1)
         assert self.p.cursor == 2048
-
 
     def test_seek_random(self):
         for _ in range(1, 3):
